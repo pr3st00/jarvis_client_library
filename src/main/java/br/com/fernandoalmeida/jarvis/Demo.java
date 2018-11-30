0100233f9141f45079952b28bd4cda21ab1cc02c
FERNANDOAlmeida <FERNANDOAlmeida@DESKTOP-BJUMM7F.pr3st00.com.br>
2018-11-30 13:52:18 -0200

    updates

--- src/main/java/br/com/fernandoalmeida/jarvis/Demo.java
+++ src/main/java/br/com/fernandoalmeida/jarvis/Demo.java
@@ -3,12 +3,21 @@ package br.com.fernandoalmeida.jarvis;
 import br.com.fernandoalmeida.jarvis.client.JarvisClient;
 import br.com.fernandoalmeida.jarvis.exception.JarvisConfigurationException;
 
+/**
+ * Demo application
+ * 
+ * @author Fernando Costa de Almeida
+ *
+ */
 public class Demo
 {
 
 	public static void main(String[] args) throws JarvisConfigurationException
 	{
 		JarvisClient client = new JarvisClient();
+		
+		// Make sure audio is enabled
+		client.enableSound();
 
 		// Say something
 		client.say("Hi there!", "Smile for the camera!");
